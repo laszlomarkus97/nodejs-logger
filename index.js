db60c05ab9fc4154f83ca2eef2ecfd83a7295ce6
Oláh István Gergely <olah.istvan.gergely@gmail.com>
2018-07-02 01:22:38 +0200

    Little cleanup

--- index.js
+++ index.js
@@ -1,21 +1,49 @@
+// create http server
 const express = require('express')
-const bodyParser = require("body-parser");
 const app = express()
 
+// just to handle post message parsing
+const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(bodyParser.json());
 
-var port = process.env.PORT || 3000;
+/* the OS can pass environment variable to the nodejs server
+   if we dont get one, use 3000 by default
+   
+   the trick here: || have lazy loading, so you can use it instead of something like this:
+   var port = 3000
+   if (process.env.PORT){
+       port = process.env.PORT
+   }
+*/
 
+var port = process.env.PORT || 3000;
+// current logs in a variable - without DB. will be lost at server restart
 var logs = [];
 
+// helper function to build our minimal page
 function buildHTML(){
 
+    html = "";
+    // concat log
+    for(var index in logs){
+        html += "<br>" + logs[index];
+    }
+    // add basic textfield
+    html += `
+    <form method="post" action="/">
+    <input type="text" name="user[name]">
+    <input type="submit" value="Submit">
+    </form>
+    `;
     return html;
 }
 
+/**
+ * If we send some data with POST to /, store it with the date
+ */
 app.post('/', (req, res) => {
     
     if(req.body.user){
@@ -23,35 +51,15 @@ app.post('/', (req, res) => {
         var msg = date.toGMTString() + ": " + req.body.user.name ;
         logs.push(msg);
     }
-    html = "";
-    for(var index in logs){
-        html += "<br>" + logs[index];
-    }
 
-    html += `
-    <form method="post" action="/">
-    <input type="text" name="user[name]">
-    <input type="submit" value="Submit">
-    </form>
-    `;
-    res.send(html)
+    res.send(buildHTML())
 });
 
-
-
+/**
+ * Just generate the current list for / GET
+ */
 app.get('/', (req, res) => {
-    var html = "";
-    for(var index in logs){
-        html += "<br>" + logs[index];
-    }
-
-    html += `
-    <form method="post" action="/">
-    <input type="text" name="user[name]">
-    <input type="submit" value="Submit">
-    </form>
-    `;
-    res.send(html)
+    res.send(buildHTML())
 })
 
 app.listen(port, () => console.log('Example app listening on port ' + port))
